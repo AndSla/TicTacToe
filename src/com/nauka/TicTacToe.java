5c4a496eba1f73fe44194040f3e16b41e952241d
Andrzej Slawek <annddrzej@gmail.com>
2020-11-09 20:32:02 +0100

    Add drawGameBoard method

--- src/com/nauka/TicTacToe.java
+++ src/com/nauka/TicTacToe.java
@@ -1,9 +1,46 @@
 package com.nauka;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter cells: ");
+        String fields = sc.nextLine();
+
+        drawGameBoard(fields);
+
+
     }
-    
+
+    public static void drawGameBoard(String fields) {
+        char[][] gameBoard = new char[3][3];
+        int k = 0;
+
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length; j++) {
+                if (fields.charAt(k) != '_') {
+                    gameBoard[i][j] = fields.charAt(k);
+                } else {
+                    gameBoard[i][j] = ' ';
+                }
+                k++;
+            }
+        }
+
+        System.out.println("---------");
+
+        for (int i = 0; i < gameBoard.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < gameBoard.length; j++) {
+                System.out.print(gameBoard[i][j] + " ");
+            }
+            System.out.println(" |");
+        }
+
+        System.out.println("---------");
+    }
+
 }
